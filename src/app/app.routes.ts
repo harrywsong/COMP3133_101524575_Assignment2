6521f830db4ac8991682187ce0d99fae0c8f54cb
Woohyuk Song <harrywsong@naver.com>
2026-04-12 13:53:49 -0400

    create app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { EmployeeListComponent } from './components/employee-list/employee-list.component';
+import { EmployeeAddComponent } from './components/employee-add/employee-add.component';
+import { EmployeeDetailComponent } from './components/employee-detail/employee-detail.component';
+import { EmployeeEditComponent } from './components/employee-edit/employee-edit.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'employees', component: EmployeeListComponent },
+  { path: 'employees/add', component: EmployeeAddComponent },
+  { path: 'employees/:id', component: EmployeeDetailComponent },
+  { path: 'employees/:id/edit', component: EmployeeEditComponent },
+  { path: '**', redirectTo: '/login' }
+];
