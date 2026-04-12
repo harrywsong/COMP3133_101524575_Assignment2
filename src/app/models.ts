a363de5320227c7c1a2f65cbc559b08e6a448c87
Woohyuk Song <harrywsong@naver.com>
2026-04-12 13:38:11 -0400

    create models.ts

---
+++ src/app/models.ts
@@ -0,0 +1,18 @@
+export interface User {
+  _id: string;
+  username: string;
+  email: string;
+}
+
+export interface Employee {
+  _id: string;
+  first_name: string;
+  last_name: string;
+  email: string;
+  gender: string;
+  designation: string;
+  salary: number;
+  date_of_joining: string;
+  department: string;
+  employee_photo?: string;
+}
