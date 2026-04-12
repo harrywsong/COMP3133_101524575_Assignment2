3d87e6c510c785b6814359f2d75d3abd8309b640
Woohyuk Song <harrywsong@naver.com>
2026-04-12 13:37:36 -0400

    edit app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,28 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
+import { InMemoryCache } from '@apollo/client/core';
+import { Apollo, APOLLO_OPTIONS } from 'apollo-angular';
+import { HttpLink } from 'apollo-angular/http';
 import { routes } from './app.routes';
+
+export function apolloOptionsFactory(httpLink: HttpLink) {
+  return {
+    link: httpLink.create({ uri: 'http://localhost:4000/graphql' }),
+    cache: new InMemoryCache()
+  };
+}
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
+    provideRouter(routes),
+    provideHttpClient(),
+    Apollo,
+    {
+      provide: APOLLO_OPTIONS,
+      useFactory: apolloOptionsFactory,
+      deps: [HttpLink]
+    }
   ]
 };
